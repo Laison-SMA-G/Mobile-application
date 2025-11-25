08ef8f2ea0853ab15910c4262ebb417dba3a5e1c
Laison-SMA-G <migelmickael729@gmail.com>
2025-11-25 17:30:42 +0800

    schema

--- PCREXSHOP/backend/models/Product.js
+++ PCREXSHOP/backend/models/Product.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+
+});
+
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: String  },
+    category: categorySchema,
     quantity: { type: Number, default: 0 },
 
     image: String,
